b486a339f845a94cff66eeec6ce7ece17bd112ff
JordanLim-exe <JLim@student.neumont.edu>
2020-02-28 14:32:40 -0700

    Stubbed out view Package

--- src/view/RPGDisplay.java
+++ src/view/RPGDisplay.java
@@ -4,10 +4,30 @@ public class RPGDisplay {
 
     public static String startDisplay() {
         //TODO Implement method
-        return "METHOD NOT IMPLEMENTED";
+        String returnMessage = "METHOD NOT IMPLEMENTED";
+        return returnMessage;
     }
 
     public static void printMap() {
-
+        //TODO Implement method
     }
+
+    public static void displayInventory() {
+        //TODO Implement method
+    }
+
+    public static String promptForDirection() {
+        //TODO Implement method
+        String returnMessage = "METHOD NOT IMPLEMENTED";
+        return returnMessage;
+    }
+
+    public static void printBattleMenu() {
+        //TODO Implement method
+    }
+
+    public static void displayMessage(String msg) {
+        //TODO Implement method
+    }
+
 }
