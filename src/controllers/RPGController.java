1c4e257766e517e0e2d0d7d9f0cd771b9b849040
JordanLim-exe <JLim@student.neumont.edu>
2020-03-03 13:53:12 -0700

    Stubbed out Map and hard-coded the main map

--- src/controllers/RPGController.java
+++ src/controllers/RPGController.java
@@ -1,10 +1,41 @@
 package controllers;
 
+import models.Enemy;
+import models.Map;
+import models.Player;
+
 public class RPGController {
 
-    public static void run() {
+    private Player player1;
+    private Enemy currentEnemy;
+    private Map map;
+
+    public void run() {
+        runStartUp();
+        boolean gameContinues = true;
+
+        do{
+
+        }while(gameContinues);
+    }
+
+    public void runStartUp() {
 
     }
 
+    public int runTurn() {
 
+    }
+
+    public void battleStart(int bossDifficulty) {
+
+    }
+
+    public void battleLoop() {
+
+    }
+
+    public boolean result() {
+
+    }
 }
